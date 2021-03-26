1878f3977df35d41f23e319503d5d83afa01d35c
Vishakha Adtani <74194073+vishadtani21@users.noreply.github.com>
2021-03-26 23:26:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+function setup() {
+  createCanvas(400,400);
+  angleMode (DEGREES);
+}
+
+function draw() {
+  background(0);  
+
+  var hr = hour();
+  var mn = minute();
+  var sc = second();
+
+  translate (200,200);
+  rotate (-90);
+
+  //seconds
+  strokeWeight (7);
+  stroke ("blue");
+  noFill ();
+  var sec = map(sc,0,60,0,360);
+  arc (0,0,300,300,0,sec);
+
+ push ();
+ rotate (sec);
+ strokeWeight(7);
+ stroke ("blue");
+ line (0,0,100,0);
+ pop ();
+
+ //minutes
+ strokeWeight (8);
+ stroke ("green");
+ noFill ();
+ var min = map (mn,0,60,0,360);
+ arc (0,0,280,280,0,min);
+
+ push ();
+ rotate (min);
+ strokeWeight (7);
+ stroke ("green");
+ line (0,0,75,0);
+ pop ();
+
+ //hours
+ strokeWeight (8);
+ stroke ("red");
+ noFill ();
+ var h = map (hr%12,0,12,0,360);
+ arc (0,0,260,260,0,h);
+
+ push ();
+ rotate (h);
+ strokeWeight (7);
+ stroke ("red");
+ line (0,0,50,0);
+ pop ();
+
+ //point
+ push ();
+ stroke ("white");
+ point (0,0);
+ pop ();
+
+
+  drawSprites();
+}
